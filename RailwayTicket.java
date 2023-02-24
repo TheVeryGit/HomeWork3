ab8edf217205376f82f59a92ea47f93eaa64ff0c
TheVeryGit <124448969+TheVeryGit@users.noreply.github.com>
2023-02-24 18:56:01 +0300

    Update RailwayTicket.java

--- RailwayTicket.java
+++ RailwayTicket.java
@@ -1,44 +1,35 @@
 package com.example.firstproject;
 
 public class RailwayTicket {
-    // задание полей сущности
-    private String departurePoint; // место отправления
-    private String arrivalPoint; // место прибытия
-    private String departureDate; // время отправления
-    private String arrivalDate; // время прибытия
-    private String travelTime; // время пути
-    private int distance; // расстояние пути
-    private float ticketPrice; // стоимость взрослого билета
-    private int numberOfTickets; // количество билетов
+    
+    private String departurePoint; 
+    private String arrivalPoint; 
+    private String departureDate; 
+    private String arrivalDate; 
+    private String travelTime; 
+    private int distance; 
+    private float ticketPrice; 
+    private int numberOfTickets;
 
-    // создание пустого конструктора
+    
     public RailwayTicket() {
     }
-
-    // создание конструктора исходных данных (стоимость билета, количество билетов)
     public RailwayTicket(float ticketPrice, int numberOfTickets) {
-        this.ticketPrice = ticketPrice; // стоимость взрослого билета
-        this.numberOfTickets = numberOfTickets; // количество билетов
+        this.ticketPrice = ticketPrice;
+        this.numberOfTickets = numberOfTickets;
     }
-
-    // метод подсчёта стоимости взрослых билетов
     public float ticketPriceAll() {
-        return ticketPrice * numberOfTickets; // нужно умножить количество билетов на стоимость одного билета
+        return ticketPrice * numberOfTickets; 
     }
-
-    // создание геттеров и сеттеров
     public float getTicketPrice() {
         return ticketPrice;
     }
-
     public void setTicketPrice(float ticketPrice) {
         this.ticketPrice = ticketPrice;
     }
-
     public int getNumberOfTickets() {
         return numberOfTickets;
     }
-
     public void setNumberOfTickets(int numberOfTickets) {
         this.numberOfTickets = numberOfTickets;
     }
